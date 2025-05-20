e77d3f5d9006d335453ab9f13709345f92569d4c
Greg Chambers <greg.chambers@tn.gov>
2025-05-20 11:16:19 -0500

    plot color on/off doc

--- R/ggplot.R
+++ R/ggplot.R
@@ -343,6 +343,10 @@ scale_fill_tn <- function(palette = "Default", discrete = TRUE, reverse = FALSE,
   }
 }
 
+#' Disable default TN plot colors
+#'
+#' @export
+#'
 tn_ggplot_colors_off<-function(){
   options(
     ggplot2.discrete.fill= 'default'
@@ -351,6 +355,10 @@ tn_ggplot_colors_off<-function(){
   invisible()
 }
 
+#' Enable default TN plot colors
+#'
+#' @export
+#'
 tn_ggplot_colors_on<-function(){
   gcol<-c("#0022a1","#fc0a2d","#6a7f8c","#66ffd6","#d4ff40","#ff6108","#cdddf4","#2f470e","#4c90d3","#ed9924","#780000","#132742","#4f897b","#ffa799", "#c24e13","#a4ce40","#f2cd47","#ed6053" ,"#174a7c" ,"#659737")
 
